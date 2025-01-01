fbd17b4782e7dfcc2a57808f9ae0c72344f49316
Trần Hoài Bảo <hoaibaotran1104@gmail.com>
2025-01-01 23:45:11 +0700

    Using input and output function

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,7 +1,6 @@
-import { Component, computed, signal } from '@angular/core';
-import { DUMMY_USERS } from '../dummy-users';
+import { Component, computed, EventEmitter, Input, input, Output, output } from '@angular/core';
 
-const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+// const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -10,17 +9,34 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
+  // select = output<string>()
 
-  imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectUser() {
+    this.select.emit(this.id);
+  }
+
+  // avatar = input.required<string>();
+  // name = input.required<string>();
+  // imagePath = computed(() => 'assets/users/' + this.avatar())
+  // selectedUser = signal(DUMMY_USERS[randomIndex]);
+
+  // imagePath = computed(() => 'assets/users/' + this.selectedUser().avatar)
 
   // get imagePath() {
   //   return 'assets/users/' + this.selectedUser().avatar;
   // }
 
-  onSelectUser() {
-    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser.set(DUMMY_USERS[randomIndex]);
-    // this.selectedUser = DUMMY_USERS[randomIndex];
-  }
+  // onSelectUser() {
+  // const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+  // this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  // this.selectedUser = DUMMY_USERS[randomIndex];
+  // }
 }
