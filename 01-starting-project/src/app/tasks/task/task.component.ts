ccc03c0b514a0f8408afe87161b8ba24ff2361dd
Trần Hoài Bảo <hoaibaotran1104@gmail.com>
2025-01-08 21:09:58 +0700

    add task component

--- 01-starting-project/src/app/tasks/task/task.component.ts
+++ 01-starting-project/src/app/tasks/task/task.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { type Task } from './task.module';
 
 // type Task = {
@@ -18,4 +18,9 @@ import { type Task } from './task.module';
 })
 export class TaskComponent {
   @Input({ required: true }) task!: Task;
+  @Output() complete = new EventEmitter<string>();
+
+  onCompleteTask() {
+    this.complete.emit(this.task.id);
+  }
 }
